7d7d2fbb46b953876deb327271fbc897bc15fc7e
Diatoming <diatoming@gmail.com>
2017-12-30 07:59:18 +0800

    init

---
+++ AlertFilter/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  AlertFilter
+//
+//  Created by Diatoming on 12/20/17.
+//  Copyright © 2017 diatoming.com. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
